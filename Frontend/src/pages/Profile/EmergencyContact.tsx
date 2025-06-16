d1bca875f41bdb69cda5314bc0902582a2387304
Helmi <dridi.helmi07@gmail.com>
2025-06-16 15:38:13 +0100

    failedone

--- Frontend/src/pages/Profile/EmergencyContact.tsx
+++
@@ -1,5 +0,0 @@
-const emergency = () => {
-  return <h1 className="text-2xl font-semibold">emergency Reached Page</h1>;
-};
-
-export default emergency;
--- Frontend/src/pages/Profile/PersonalInfo.tsx
+++
@@ -1,5 +0,0 @@
-const personal = () => {
-  return <h1 className="text-2xl font-semibold">personal Reached Page</h1>;
-};
-
-export default personal;
